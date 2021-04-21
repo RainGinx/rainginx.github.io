d3a6e333013032dd63a25fb8d2698c682b6a5fc9
RainGinx <rainginx@gmail.com>
2021-04-21 22:48:54 +0800

    update file

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  PORT:80
+  NODE_ENV: '"development"'
 })
